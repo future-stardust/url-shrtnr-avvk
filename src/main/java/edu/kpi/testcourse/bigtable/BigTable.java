c710791f0fbe7411f1abb5a15de669f30b8b6128
furman <furman0507@gmail.com>
2021-03-11 22:25:59 +0200

    reformat code

--- src/main/java/edu/kpi/testcourse/bigtable/BigTable.java
+++ src/main/java/edu/kpi/testcourse/bigtable/BigTable.java
@@ -1,8 +1,8 @@
 package edu.kpi.testcourse.bigtable;
 
-
 // ⚠️ Please, pay attention, that you should not use any 3rd party persistence library (e.g. data
 // ⚠️ base, implementation of key-value storage, etc)
+
 import com.google.gson.JsonObject;
 import edu.kpi.testcourse.models.AuthToken;
 import edu.kpi.testcourse.models.User;
@@ -17,15 +17,22 @@ import java.util.List;
 public interface BigTable {
 
   int putUser(String email, String passHash);
+
   User getUser(String email);
+
   String getUserId(String authToken);
 
   void putToken(String userId, String authToken);
+
   AuthToken getToken(String userId);
+
   void deleteToken(String authToken);
 
   void putAlias(int userId, String alias, String link);
+
   boolean deleteAlias(String alias);
+
   UserLink getLink(String alias);
+
   List<UserLink> getAllUserLinks(String userId);
 }
