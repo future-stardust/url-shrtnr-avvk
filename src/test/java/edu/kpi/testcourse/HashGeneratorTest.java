6e24d1279135d04a704f30cc9f9e7cd2743e7a8d
Andrey <hhhandrej69@gmail.com>
2021-03-11 21:47:20 +0200

    hash generator

---
+++ src/test/java/edu/kpi/testcourse/HashGeneratorTest.java
@@ -0,0 +1,16 @@
+package edu.kpi.testcourse;
+
+import edu.kpi.testcourse.auth.HashGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HashGeneratorTest {
+  @Test
+  void testHashMd5() {
+    String password = "12345";
+    String md5Hash = "827ccb0eea8a706c4c34a16891f84e7b";
+    HashGenerator hashGenerator = new HashGenerator();
+    assertThat(hashGenerator.generate(password)).isEqualTo(md5Hash);
+  }
+}
