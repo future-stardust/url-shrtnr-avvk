d6c0e03b05aa82f5976fb3b23050055c77b4f390
Ivan Yavorsky <02ivan0971@gmail.com>
2021-03-11 22:01:28 +0200

    Add models

---
+++ src/main/java/edu/kpi/testcourse/models/User.java
@@ -0,0 +1,23 @@
+package edu.kpi.testcourse.models;
+
+public class User {
+  private int userId;
+  private String email;
+  private String userPassHash;
+
+  public User(int userId, String email, String userPassHash) {
+    this.email = email;
+    this.userPassHash = userPassHash;
+  }
+
+  public int getUserId() {return userId;}
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getUserPassHash() {
+    return userPassHash;
+  }
+
+}
---
+++ src/main/java/edu/kpi/testcourse/models/UserLink.java
@@ -0,0 +1,25 @@
+package edu.kpi.testcourse.models;
+
+public class UserLink {
+  private int userId;
+  private String shortenedLink;
+  private String redirectLink;
+
+  public UserLink(int userId, String shortenedLink, String redirectLink ) {
+    this.userId = userId;
+    this.shortenedLink = shortenedLink;
+    this.redirectLink = redirectLink;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public String getShortenedLink() {
+    return shortenedLink;
+  }
+
+  public String getRedirectLink() {
+    return redirectLink;
+  }
+}
